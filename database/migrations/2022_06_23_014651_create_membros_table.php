d963d2ce208408a81a028e38326abe03d78b251e
Bruno Mendes da Silva <88750125+Brunomtk@users.noreply.github.com>
2023-06-12 15:18:52 -0300

    Add files via upload

---
+++ database/migrations/2022_06_23_014651_create_membros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('cargo');
+            $table->string('coordenadoria');
+            $table->mediumText('foto');
+            $table->json('horario');
+            $table->boolean('presente')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membros');
+    }
+}
